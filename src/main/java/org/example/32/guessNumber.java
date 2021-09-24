8aaf4d575401c3f2fc60880ebd128478ed6192ee
chrissy-buzz <christinapersaud98@gmail.com>
2021-09-24 15:32:40 -0700

    commit 30 09/24/21

---
+++ src/main/java/org/example/32/guessNumber.java
@@ -0,0 +1,17 @@
+package org.example;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Christina Persaud
+ */
+
+import java.util.Scanner;
+import java.lang.*;
+import java.util.*;
+
+public class guessNumber {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/example/33/randomcl.java
@@ -0,0 +1,28 @@
+package example.org;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Christina Persaud
+ */
+
+import java.util.Random;
+import java.util.Scanner;
+
+class randomcl
+{
+    public static void main(String[] args)
+    {
+        Scanner scan1 = new Scanner(System.in);
+        System.out.println("What's your question?");
+        String question = scan1.next();
+        System.out.println(randomcl());
+    }
+
+    public static String randomcl()
+    {
+        String[] possibleWords = {"yes", "no", "maybe later"};
+        Random random = new Random();
+        int randomIdx = random.nextInt(possibleWords.length);
+        return possibleWords[randomIdx];
+    }
+}

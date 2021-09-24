ecd36a4ae1b8f2b32cd87e4e6cb005fe05bbd18f
chrissy-buzz <christinapersaud98@gmail.com>
2021-09-24 14:33:40 -0700

    commit 30 09/24/21

--- src/main/java/org/example/31/karvonen.java
+++ src/main/java/org/example/31/karvonen.java
@@ -9,3 +9,31 @@ import java.util.Scanner;
 import java.lang.*;
 import java.util.*;
 
+public class karvonen {
+
+    public static void main(String[] args) {
+
+        double rate=0;
+        double intensity=55;
+
+
+        Scanner scan1 = new Scanner(System.in);
+        System.out.print("Age: ");
+        int age = scan1.nextInt();
+
+        Scanner scan2 = new Scanner(System.in);
+        System.out.print("Resting Pulse: ");
+        int restingHR = scan2.nextInt();
+
+        System.out.println("Intensity   | Rate");
+        System.out.println("________________________");
+        for (int i=55; i<=95; i+=5){
+            rate= (((220-age) - restingHR) * (intensity/100) + restingHR);
+            System.out.print(i + "%\t");
+            System.out.print("\t\t|" + Math.round(rate) + " bpm\n");
+            intensity+=5;
+        }
+
+    }
+
+}
